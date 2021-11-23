d51f2353d3ec20bec3627b2e84f0ff1e4202abe1
smoggy <smoggy@alumni.sjtu.edu.cn>
2021-11-23 18:27:30 +0100

    edge visualization in rviz

--- src/planner/example/include/prm_planner.h
+++ src/planner/example/include/prm_planner.h
@@ -28,7 +28,10 @@ struct Vertice{
 class Graph{
     public:
         void insertVex(Vertice vex1);
-        void insertEdge(const Vertice & vex1, const Vertice & vex2, int cost);
+        void insertEdge(const int & vex1, const int & vex2, int cost);
+        int get_numVex(){return numVex;};
+        int get_numEdge(){return numEdge;};
+        vector<Vertice> get_vexList(){return VexList;};
     private:
         vector<Vertice> VexList;
         int numVex = 0, numEdge = 0;
@@ -41,21 +44,24 @@ class PRM{
         PRM();
         void callback(const geometry_msgs::PoseStamped::ConstPtr& msg);
         void planner_visualization();
-        void node_visualization();
+        void node_generation();
+        void edge_generation();
         void get_map_param();
     private:
         ros::NodeHandle nh_;
         //subscribe from topic /move_base_simple/goal to get the destination
         ros::Subscriber sub_;
         //publisher the visulization of the algorithm
-        ros::Publisher pub_;
-        ros::Publisher prm_pub_;
+        ros::Publisher edge_pub_;
+        ros::Publisher node_pub_;
         //start and end point 
         geometry_msgs::Point start,goal;
         //map size
         double map_size_x,map_size_y,map_size_z;
         //sample number
         int n_sample;
+        //graph
+        Graph graph_;
 };
 
 #endif
--- src/planner/example/src/prm_planner.cpp
+++ src/planner/example/src/prm_planner.cpp
@@ -22,45 +22,35 @@ void Graph::insertVex(Vertice vex1){
     numVex++;
 
 }
-void Graph::insertEdge(const Vertice & vex1, const Vertice & vex2, int cost){
+void Graph::insertEdge(const int & vex1, const int & vex2, int cost){
+    Edge * new_edge1 = new Edge(vex1, vex2, cost);
+    Edge * new_edge2 = new Edge(vex1, vex2, cost);
+    numEdge++;
 
-    //find vertexs in list
-    auto itr1 = find(VexList.begin(), VexList.end(), vex1);
-    auto itr2 = find(VexList.begin(), VexList.end(), vex2);
-    
-    if(itr1==VexList.end() || itr2==VexList.end())cout<<"insert edge failed, cannot find vertex"<<endl;
+    //add created edge to edge list stored in vertex
+    if(VexList[vex1].FirstAdjacentEdge==nullptr)VexList[vex1].FirstAdjacentEdge = new_edge1;
     else{
-
-        //create new edge
-        Edge * new_edge1 = new Edge(distance(VexList.begin(), itr1), distance(VexList.begin(), itr2), cost);
-        Edge * new_edge2 = new Edge(distance(VexList.begin(), itr1), distance(VexList.begin(), itr2), cost);
-        numEdge++;
-
-        //add created edge to edge list stored in vertex
-        if(itr1->FirstAdjacentEdge==nullptr)itr1->FirstAdjacentEdge = new_edge1;
-        else{
-            Edge* ptr = itr1->FirstAdjacentEdge;
-            while(ptr->next!=nullptr)ptr = ptr->next;
-            ptr->next = new_edge1;
-        }
-        if(itr2->FirstAdjacentEdge==nullptr)itr2->FirstAdjacentEdge = new_edge2;
-        else{
-            Edge* ptr = itr2->FirstAdjacentEdge;
-            while(ptr->next!=nullptr)ptr = ptr->next;
-            ptr->next = new_edge2;
-        }
+        Edge* ptr = VexList[vex1].FirstAdjacentEdge;
+        while(ptr->next!=nullptr)ptr = ptr->next;
+        ptr->next = new_edge1;
+    }
+    if(VexList[vex2].FirstAdjacentEdge==nullptr)VexList[vex2].FirstAdjacentEdge = new_edge2;
+    else{
+        Edge* ptr = VexList[vex2].FirstAdjacentEdge;
+        while(ptr->next!=nullptr)ptr = ptr->next;
+        ptr->next = new_edge2;
     }
 }
 
+//member function for PRM planner
 PRM::PRM() {
   sub_ = nh_.subscribe("/move_base_simple/goal", 5, &PRM::callback, this);
-  pub_ = nh_.advertise<visualization_msgs::Marker>("visualization_marker", 10);
-  prm_pub_ = nh_.advertise<visualization_msgs::Marker>("prm_markers", 10);
+  edge_pub_ = nh_.advertise<visualization_msgs::Marker>("edge_marker", 10);
+  node_pub_ = nh_.advertise<visualization_msgs::Marker>("node_markers", 10);
 
   get_map_param();
 //   node_visualization();
 }
-
 void PRM::get_map_param() {
   if (nh_.getParam("/random_forest/map/x_size", map_size_x)) {
     ROS_INFO("get map x: %f", map_size_x);
@@ -75,31 +65,63 @@ void PRM::get_map_param() {
     ROS_INFO("get sample number: %i", n_sample);
   }
 }
-
 // Visualization sample nodes
-void PRM::node_visualization() {
-  // generate random node
-  visualization_msgs::Marker points_;
-  points_.header.frame_id = "world";
-  points_.header.stamp = ros::Time::now();
-  points_.ns = "planner_example";
-  points_.action = visualization_msgs::Marker::ADD;
-  points_.pose.orientation.w = 1.0;
-  points_.id = 0;
-  points_.type = visualization_msgs::Marker::POINTS;
-  points_.scale.x = 0.2;
-  points_.scale.y = 0.2;
-  points_.color.g = 1.0f;
-  points_.color.a = 1.0;
-  for (int i = 0; i < n_sample; i++) {
-    geometry_msgs::Point p;
-    p.x = ((double)rand() / (RAND_MAX)-0.5) * map_size_x;
-    p.y = ((double)rand() / (RAND_MAX)-0.5) * map_size_y;
-    p.z = ((double)rand() / (RAND_MAX)) * map_size_z;
-    ROS_INFO("X:%f  Y:%f  Z:%f", p.x, p.y, p.z);
-    points_.points.push_back(p);
-  }
-  prm_pub_.publish(points_);
+void PRM::node_generation() {
+    // generate random node
+    visualization_msgs::Marker points_;
+    points_.header.frame_id = "world";
+    points_.header.stamp = ros::Time::now();
+    points_.ns = "planner_example";
+    points_.action = visualization_msgs::Marker::ADD;
+    points_.pose.orientation.w = 1.0;
+    points_.id = 0;
+    points_.type = visualization_msgs::Marker::POINTS;
+    points_.scale.x = 0.2;
+    points_.scale.y = 0.2;
+    points_.color.g = 1.0f;
+    points_.color.a = 1.0;
+    for (int i = 0; i < n_sample; i++) {
+        geometry_msgs::Point p;
+        p.x = ((double)rand() / (RAND_MAX)-0.5) * map_size_x;
+        p.y = ((double)rand() / (RAND_MAX)-0.5) * map_size_y;
+        p.z = ((double)rand() / (RAND_MAX)) * map_size_z;
+        ROS_INFO("X:%f  Y:%f  Z:%f", p.x, p.y, p.z);
+        Vertice v(p.x,p.y,p.z);
+        graph_.insertVex(v);
+        points_.points.push_back(p);
+    }
+    node_pub_.publish(points_);
+}
+
+void PRM::edge_generation() {
+    visualization_msgs::Marker line_list;
+    line_list.header.frame_id = "world";
+    line_list.header.stamp = ros::Time::now();
+    line_list.ns = "planner_example";
+    line_list.action = visualization_msgs::Marker::ADD;
+    line_list.pose.orientation.w = 1.0;
+    line_list.id = 2;
+    line_list.type = visualization_msgs::Marker::LINE_LIST;
+    line_list.scale.x = 0.1;
+    line_list.color.r = 1.0;
+    line_list.color.a = 1.0;
+
+    for(int i=0;i<graph_.get_numVex();i++){
+        for(int j=i+1;j<graph_.get_numVex();j++){
+            graph_.insertEdge(i,j,1);
+            geometry_msgs::Point p1,p2;
+            p1.x = graph_.get_vexList()[i].x;
+            p1.y = graph_.get_vexList()[i].y;
+            p1.z = graph_.get_vexList()[i].z;
+            p2.x = graph_.get_vexList()[j].x;
+            p2.y = graph_.get_vexList()[j].y;
+            p2.z = graph_.get_vexList()[j].z;
+            line_list.points.push_back(p1);
+            line_list.points.push_back(p2);
+        }
+    }
+
+    edge_pub_.publish(line_list);
 }
 
 void PRM::callback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
@@ -107,23 +129,6 @@ void PRM::callback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
   this->start.y = 0;
   this->start.z = 0;
   this->goal = msg->pose.position;
-  PRM::planner_visualization();
-  node_visualization();
-}
-
-void PRM::planner_visualization() {
-  visualization_msgs::Marker line_strip;
-  line_strip.header.frame_id = "world";
-  line_strip.header.stamp = ros::Time::now();
-  line_strip.type = visualization_msgs::Marker::LINE_STRIP;
-  line_strip.action = visualization_msgs::Marker::ADD;
-  line_strip.ns = "planner_example";
-  line_strip.pose.orientation.w = 1.0;
-  line_strip.scale.x = 0.1;
-  line_strip.color.b = 1.0;
-  line_strip.color.a = 1.0;
-  line_strip.id = 1;
-  line_strip.points.push_back(start);
-  line_strip.points.push_back(goal);
-  pub_.publish(line_strip);
+  node_generation();
+  edge_generation();
 }
